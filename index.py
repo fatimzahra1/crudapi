ab7a24e0ce1373f6272bbb114d8c3a8be3687317
fatimzahra1 <fatimzahraelmadini@gmail.com>
2021-11-29 16:31:44 +0100

    link api to frontend

--- index.py
+++ index.py
@@ -3,7 +3,7 @@ from routes.student import student_router
 from fastapi.middleware.cors import CORSMiddleware
 
 
-client_apps = ['http://localhost:3000']#Our REACT app will be running on this IP and PORT
+client_apps = ['https://mystifying-bell-ab0aa8.netlify.app/']#Our REACT app will be running on this IP and PORT
 
 #Create app
 app = FastAPI()
@@ -11,10 +11,10 @@ app = FastAPI()
 app.include_router(student_router)
 
 #Register App with CORS middleware to allow resourse sharing between different domains/origins
-# app.add_middleware(
-#     CORSMiddleware,
-#     allow_origins=client_apps,
-#     allow_credentials=True,
-#     allow_methods=["*"],
-#     allow_headers=["*"]
-# )
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=client_apps,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
